d43ca9a84ee9fb48bae90eab9e21673f67a1e90b
varinder <varinde.gupta@tcs.com>
2018-07-22 08:07:46 +0530

    updated changes

---
+++ run_local.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+
+if [[ -z "${CATALINA_HOME}" ]]; then
+  echo="CATALINA_HOME environment value is mandatory.."
+  exit;
+else
+  echo="CATALINA_HOME environment value is set $CATALINA_HOME"
+fi
+
+# create artifact
+cd ./shoppingcart-rest-service
+mvn install
+cp target/shoppingcartservice-SNAPSHOT.war $CATALINA_HOME/webapps/shoppingcartservice.war
+
+cd ../
+
+cd ./reactjs-shopping-cart
+npm install
+npm start -Y
